c5ca22623b266c73c57ceba051381c8b4fa145b5
Cyprian Zdebski <cyprian.zdebski@protonmail.com>
2022-11-04 00:24:56 +0100

    Patched: focusmaster-return

--- config.def.h
+++ config.def.h
@@ -71,6 +71,7 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY|ShiftMask,             XK_Return, focusmaster,    {0} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
