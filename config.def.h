63fcf212512c974c7e45ac3778164753939ac655
Cyprian Zdebski <cyprian.zdebski@protonmail.com>
2022-11-04 03:55:22 +0100

    Changed the default gap settings

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 20};
+static const Gap default_gap        = {.isgap = 0, .realgap = 0, .gappx = 0};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
