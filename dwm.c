b2d7e21cc0a28a8e7ed92a3837f1c002915faba2
Cyprian <cyprianz5mail@gmail.com>
2023-02-16 18:15:56 +0100

    Patched: statusallmons

--- dwm.c
+++ dwm.c
@@ -785,7 +785,7 @@ drawbar(Monitor *m)
 		return;
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon) { /* status is only drawn on selected monitor */
+	if (m == selmon || 1) { /* status is only drawn on selected monitor */
 		char *text, *s, ch;
 		drw_setscheme(drw, scheme[SchemeNorm]);
 
@@ -2327,6 +2327,7 @@ updatesizehints(Client *c)
 void
 updatestatus(void)
 {
+  Monitor* m;
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext))) {
 		strcpy(stext, "dwm-"VERSION);
 		statusw = TEXTW(stext) - lrpad + 2;
@@ -2346,7 +2347,7 @@ updatestatus(void)
 		statusw += TEXTW(text) - lrpad + 2;
 
 	}
-	drawbar(selmon);
+	for(m = mons; m; m = m->next) drawbar(m);
 }
 
 void
